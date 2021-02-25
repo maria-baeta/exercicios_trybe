f5de46b1c6b82685fedd34c5d4f77f2737f94c4e
Maria Baeta <m.martinsdg@gmail.com>
2021-02-25 16:21:16 -0300

    Exercicios bloco4 dia2

---
+++ introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
@@ -0,0 +1,35 @@
+//let lista = ['Trabalho', 'Casa - Estudos', 'Casa - Janta'];
+//console.log(lista.length);
+
+//let exibeItensLista = lista[1];
+//console.log(exibeItensLista);
+
+//lista.push ('Estudar');
+
+//lista.unshift('Acordar');
+//console.log(lista)
+
+//lista.pop();
+//console.log(lista)
+
+//let buscaNaLista = lista.indexOf('Trabalho')
+//console.log(buscaNaLista)
+
+//let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+//let menuServices = menu[1];
+
+//console.log(menuServices);
+
+//let indexOfPortfolio = menu.indexOf('Portfólio');
+//console.log(indexOfPortfolio);
+//menu.push('Contato');
+//console.log(menu);
+
+//let carros= ["Saab", "Volvo", "BMW"];
+//for (let i = 0; i < carros.length; i++) {
+  //  console.log(carros[i])}
+  let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+
+  for (let index = 0; index < groceryList.length; index++){
+      console.log(groceryList[index])
+}
