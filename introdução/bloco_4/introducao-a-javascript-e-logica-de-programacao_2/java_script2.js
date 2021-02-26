6a0d868f2e6dfbab728139111ecfd01c712065c3
Maria Baeta <m.martinsdg@gmail.com>
2021-02-25 22:07:03 -0300

    Adiciona exercicio 4

--- introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/java_script2.js
+++ introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/java_script2.js
@@ -21,3 +21,8 @@ for (let index = 0; index < numbers.length; index++) {
 }
 let mediaArit = totalSoma / numbers.length
 console.log(mediaArit)
+
+//4.Com o mesmo código do exercício anterior, caso valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
+if (mediaArit > 20) {
+    console.log('Valor maior que 20')
+} else { console.log('Valor menor ou igual a 20')}
