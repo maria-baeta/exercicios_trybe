97b55cf45a28ff2ede8d43c77c52554c1292e37c
Maria Baeta <m.martinsdg@gmail.com>
2021-02-25 21:59:58 -0300

    Adiciona os Exercicios 2 e 3

--- introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/java_script2.js
+++ introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/java_script2.js
@@ -1,3 +1,23 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 //1.Nesse primeiro exercício, percorra o array imprimindo todos os valores nele contidos com a função console.log() ;
 console.log(numbers)
+
+//2.Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let total = 0;
+for (let index = 0; index < numbers.length; index++) {
+    total += numbers[index];
+}
+console.log(total)
+
+//3.Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let totalSoma = 0;
+for (let index = 0; index < numbers.length; index++) {
+    totalSoma += numbers[index];
+}
+let mediaArit = totalSoma / numbers.length
+console.log(mediaArit)
