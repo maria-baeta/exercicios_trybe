045da320dfaa9e998cf8107d8e9591652b584590
Maria Baeta <m.martinsdg@gmail.com>
2021-02-26 22:31:47 -0300

    Adiciona os exercicios 1 e 2

---
+++ introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/java_script.js
@@ -0,0 +1,20 @@
+//1- Para o primeiro exercício de hoje, faça um programa que, dado um valor n qualquer, seja n > 1 , imprima na tela um quadrado feito de asteriscos de lado de tamanho n . Por exemplo:
+let n = 5;
+let simbolo = '*'
+let resultado = ''; 
+for (let index = 0; index < n; index += 1) {
+  resultado += simbolo
+}
+for (let index = 0; index < n; index += 1) {
+  console.log(resultado)
+}
+//2. Para o segundo exercício, faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base.
+
+let n = 5;
+let simbolo = '*'
+let resultado = ''; 
+for (let index = 0; index < n; index += 1) {
+  console.log(resultado += simbolo)
+}
+
+//3.Agora inverta o lado do triângulo. 
