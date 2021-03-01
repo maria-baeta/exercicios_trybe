d098dab4ef54f4134495f0292fade77d54d71382
Maria Baeta <m.martinsdg@gmail.com>
2021-03-01 19:06:24 -0300

    Exercicio de fixaçao partes 3 e 4

--- introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/java-scripts.js
+++ introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/java-scripts.js
@@ -3,6 +3,25 @@ let info = {
     origem: "Pato Donald",
     nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
   };
+
   console.log("Bem vinda, " + info.personagem);
+  
   info ['recorrente'] = 'Sim';
+  
   console.log(info)
+  
+  for(let key in info) {
+      console.log(key)
+  }
+
+
+let info = {
+  personagem: "Margarida",
+  origem: "Pato Donald",
+  nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+};
+  info ['recorrente'] = 'Sim';
+  
+  for(let key in info) {
+    console.log(info[key]);
+}
