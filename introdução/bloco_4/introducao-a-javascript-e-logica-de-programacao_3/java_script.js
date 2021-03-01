48211eb38d66d5265d19bdee5e4fb5883fe5c44e
Maria Baeta <m.martinsdg@gmail.com>
2021-03-01 18:53:34 -0300

    Organizar os push

--- introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/java_script.js
+++ introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_3/java_script.js
@@ -18,3 +18,28 @@ for (let index = 0; index < n; index += 1) {
 }
 
 //3.Agora inverta o lado do triângulo. 
+
+let n = 5;
+let simbolo = '*';
+let espaçoVazio = ' ';
+for (let index = 1; index <= n; index += 1) {
+  let resultado = '';
+  for (let index2 = 0; index2 < (n - index); index2 += 1) {
+    resultado += espaçoVazio;
+  }
+  for (let index3 = 0; index3 < index; index3 += 1) {
+    resultado += simbolo;
+  }
+  console.log(resultado);
+}
+
+// 4- Depois, faça uma pirâmide com n asteriscos de base:
+let base = 5;
+let simbol = "*";
+let emptySpace = " ";
+for (let index =2; index <= base; index += 1) {
+  result = " "
+  console.log(result += emptySpace)
+  
+
+}
