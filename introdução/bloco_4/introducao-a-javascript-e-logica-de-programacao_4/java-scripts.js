d0d9254e7a08064ac82016417ba34ab10861649d
Maria Baeta <m.martinsdg@gmail.com>
2021-03-01 19:16:09 -0300

    Exercicio de fixaçao partes 5

--- introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/java-scripts.js
+++ introdução/bloco_4/introducao-a-javascript-e-logica-de-programacao_4/java-scripts.js
@@ -25,3 +25,15 @@ let info = {
   for(let key in info) {
     console.log(info[key]);
 }
+
+let info2 = {
+  personagem: "Tio Patinhas",
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: "O último MacPatinhas",
+  recorrente: "Sim"
+};
+
+console.log(info.personagem + " e " + info2.personagem)
+console.log(info.origem + ' e ' + info2.origem)
+console.log(info.nota + ' e ' + info2.nota)
+console.log(info.recorrente)
