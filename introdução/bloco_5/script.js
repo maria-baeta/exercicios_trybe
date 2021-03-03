8429d31141de39fb604c088dd94a67b5f56afa9a
Maria Baeta <m.martinsdg@gmail.com>
2021-03-03 20:54:49 -0300

    Execucao do exercicio de fixacao do bloco 5.1

---
+++ introdução/bloco_5/script.js
@@ -0,0 +1,8 @@
+document.getElementById('header-container').style.backgroundColor = 'rgb(76,164,109)';
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor ='pink';
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor ='yellow';
+document.getElementsByTagName('h3')[0].style.backgroundColor = 'purple';
+document.getElementsByTagName('h3')[1].style.backgroundColor = 'purple';
+document.getElementsByTagName('h3')[2].style.backgroundColor = 'black';
+document.getElementsByTagName('h3')[3].style.backgroundColor = 'black';
+document.getElementById('footer-container').style.backgroundColor = 'rgb(76,164,109)';
