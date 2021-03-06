29af9f6cb646bbcc264ddc2a9e76dc0e762a70b6
Maria Baeta <m.martinsdg@gmail.com>
2021-03-06 13:19:16 -0300

    Adiciona o botao feriado

--- introdução/bloco_5/dia_3/script.js
+++ introdução/bloco_5/dia_3/script.js
@@ -13,4 +13,38 @@ function createDaysOfTheWeek() {
   
   createDaysOfTheWeek();
   
-  // Escreva seu código abaixo.
+  // Exercicio 1
+  const recuperaIdDays = document.querySelector("#days");
+  console.log(recuperaIdDays)
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  for (let index = 0; index< dezDaysList.length; index +=1){
+    let saveDay = dezDaysList[index];
+    let createElementList = document.createElement("li")
+    if (saveDay === 24 | saveDay === 31){
+      createElementList.className = "day holiday";
+      createElementList.innerHTML = saveDay;
+      recuperaIdDays.appendChild(createElementList)
+    }  else if (saveDay === 4 | saveDay === 11 | saveDay === 18){
+      createElementList.className = "day friday";
+      createElementList.innerHTML = saveDay;
+      recuperaIdDays.appendChild(createElementList)
+    } else if (saveDay === 25){
+      createElementList.className = "day holiday friday";
+      createElementList.innerHTML = saveDay;
+      recuperaIdDays.appendChild(createElementList)
+    } else {
+      createElementList.className = "day";
+    createElementList.innerText = saveDay;
+    recuperaIdDays.appendChild(createElementList)
+    }
+  }
+
+ //Exercicio 2
+function createButtonHoliday (string){
+let buttonContainer = document.querySelector('.buttons-container')
+let newButtonHoliday = document.createElement('button')
+newButtonHoliday.innerHTML  = string;
+newButtonHoliday.id = 'btn-holiday'
+buttonContainer.appendChild(newButtonHoliday);
+}
+createButtonHoliday('Feriados');
