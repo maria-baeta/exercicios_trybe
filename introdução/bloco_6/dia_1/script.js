6da799aba97bd49ad229eecd38b6d5fee7659d63
Maria Baeta <m.martinsdg@gmail.com>
2021-03-15 22:50:59 -0300

    Cria os buttons

--- introdução/bloco_6/dia_1/script.js
+++ introdução/bloco_6/dia_1/script.js
@@ -3,11 +3,9 @@ function createOption() {
   const statesBR = ['Acre', 'Alagoas', 'Amapá','Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão', 'Mato Grosso',  'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins', 'Distrito Federal'];
   for (let index = 0; index < statesBR.length; index += 1) {
     let stateBr = statesBR[index]
-    let valueState = statesBR[index].valueOf()
     const state = document.querySelector('#state');
     const createOption = document.createElement('option');
     createOption.innerHTML = stateBr;
-    
     state.appendChild(createOption);
   }
 }
