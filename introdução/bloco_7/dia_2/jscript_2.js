e147b8d9903f626d30a6ab11ddd21fc6276c267f
Maria Baeta <m.martinsdg@gmail.com>
2021-03-24 20:39:23 -0300

    Atende ao exercício de fixação 3 - bloco 7 - dia 2

--- introdução/bloco_7/dia_2/jscript_2.js
+++ introdução/bloco_7/dia_2/jscript_2.js
@@ -17,3 +17,15 @@ const lesson3 = {
   professor: 'Maria Clara',
   turno: 'noite'
 }
+
+// 1. Crie uma função para adicionar o turno da manhã na lesson2 . Essa função deve possuir três parâmetros, sendo eles: o objeto a ser modificado, a chave que deverá ser adicionada e o valor dela.
+
+const createTurno = () => {
+  let keyNewTurno = 'turno';
+  const horário = 'manhã';
+   lesson2[keyNewTurno] = horário;
+  
+}
+createTurno()
+
+console.log(lesson2)
