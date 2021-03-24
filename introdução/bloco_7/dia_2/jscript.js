b1f7175086da036b3e938515520d5b28c9553ca5
Maria Baeta <m.martinsdg@gmail.com>
2021-03-24 18:33:26 -0300

    Atende ao exercício de fixação 1 - bloco 7 - dia 2

--- introdução/bloco_7/dia_2/jscript.js
+++ introdução/bloco_7/dia_2/jscript.js
@@ -37,15 +37,51 @@ const order = {
 const customerInfo = (order) => {
   //Escreva aqui seu código
   // Complete a função customerInfo() para que seu retorno seja similar a "Olá Ana Silveira, entrega para: Rafael Andrade, Telefone: 11-98763-1416, R. Rua das Flores, Nº: 389, AP: 701".
-  
+  const deliveryPerson = Object.values(order.order.delivery)
+  let personDelivery = ' ';
+  for (personDelivery in deliveryPerson){
+    personDelivery = deliveryPerson[0]
+  };
 
+  const person = Object.values(order)
+  let personEntrega = '';
+  let phone = ' '; 
+    for (personEntrega in person) {
+    if (person[0]){
+      personEntrega = person[0]
+    }
+    if (person[1]) {
+      phone = person[1]
+    }
+  };
+  
+  const addressComplet = Object.values(order.address)
+  let addressStreet = ' ';
+  let addressNum = ' ';
+  let addressApt = ' ';
+  for (addressStreet in addressComplet) {
+    if (addressComplet[0]) {
+      addressStreet = addressComplet[0]
+    }
+    if (addressComplet[1]) {
+      addressNum = addressComplet[1]
+    }
+    if (addressComplet[2]) {
+      addressApt = addressComplet[2]
+    }
+
+  }
+  // console.log(addressApt)
+
+  console.log((`Olá ${personDelivery}, entrega para: ${personEntrega}, Telefone ${phone}, R ${addressStreet}, Nº ${addressNum}, Ap: ${addressApt} `))
+  
 }
 
 customerInfo(order);
 
-const orderModifier = (order) => {
-  //Escreva aqui seu código
-}
+// const orderModifier = (order) => {
+//   //Escreva aqui seu código
+// }
 
-orderModifier(order);
+// orderModifier(order);
 
