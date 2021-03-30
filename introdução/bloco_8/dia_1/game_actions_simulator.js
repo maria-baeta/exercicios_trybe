659531daddf971136481d1af01362f8abd95e837
Maria Baeta <m.martinsdg@gmail.com>
2021-03-30 00:29:26 -0300

    Inicia a 2º do exercício bonus

--- introdução/bloco_8/dia_1/game_actions_simulator.js
+++ introdução/bloco_8/dia_1/game_actions_simulator.js
@@ -73,3 +73,14 @@ if (manaGasta > 15) {
 }
 }
 console.log(attack(mage));
+
+//PARTE 2 
+// 1. Crie a primeira HOF que compõe o objeto gameActions . Ela será a função que simula o turno do personagem warrior . Esta HOF receberá como parâmetro a função que calcula o dano deferido pelo personagem warrior e atualizará os healthPoints do monstro dragon . Além disto ela também deve atualizar o valor da chave damage do warrior .
+
+const gameActions = {
+  // Crie as HOFs neste objeto.
+  turnoAttack: () => {
+
+  },
+  turnoAttack()
+};
