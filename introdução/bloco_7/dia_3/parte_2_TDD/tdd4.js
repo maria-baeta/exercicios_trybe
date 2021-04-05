764f089ba4041aa1a3b068166d98311207deac3f
Maria Baeta <m.martinsdg@gmail.com>
2021-04-05 13:56:19 -0300

    Exercicio de fixação 4

--- introdução/bloco_7/dia_3/parte_2_TDD/tdd4.js
+++ introdução/bloco_7/dia_3/parte_2_TDD/tdd4.js
@@ -2,32 +2,29 @@
 
 const assert = require('assert');
 // escreva a função findTheNeedle aqui
-const findTheNeedle = (words, word ) => {
-  let resultado = 0;
-  for (let index = 0; index < words.length; index += 1) {
-    if (words[index] === word) {
-      words[index]
-    }
-  //   } else {
-  //     resultado = -1
-  //   }
-  // }
-  return  console.log(words.index)
-}
-findTheNeedle(['house', 'train', 'slide', 'needle', 'book'], 'needle');
+const findTheNeedle = (array, string) => {
+let resultado = -1;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] === string) {
+      resultado = Number(index);
+    };
+  };
+  return resultado
+};
+console.log(findTheNeedle(['house', 'train', 'slide', 'needle', 'book'], 'needle'))
 
 
 let words = ['house', 'train', 'slide', 'needle', 'book'];
 let expected = 3;
 let output = findTheNeedle(words, 'needle');
-// assert.strictEqual(output, expected);
+assert.strictEqual(output, expected);
 
 words = ['plant', 'shelf', 'arrow', 'bird'];
 expected = 0;
 output = findTheNeedle(words, 'plant');
-// assert.strictEqual(output, expected);
+assert.strictEqual(output, expected);
 
 words = ['plant', 'shelf', 'arrow', 'bird'];
 expected = -1;
 output = findTheNeedle(words, 'plat');
-// assert.strictEqual(output, expected);
+assert.strictEqual(output, expected);
